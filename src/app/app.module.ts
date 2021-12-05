d93b3fe35a5b208b35028bc206eecf80601e350b
leonel1221 <lzm_21@hotmail.com>
2021-12-05 13:04:23 -0500

    trabajo en Angular agregar navegacion terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,25 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { MessagesComponent } from './messages/messages.component'; // <-- NgModel lives here
+import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component'; // <-- NgModel lives here
 
 
 
 @NgModule({
+  imports: [
+    BrowserModule,
+
+    FormsModule,
+    AppRoutingModule
+  ],
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
-  ],
-  imports: [
-    BrowserModule,
-
-    FormsModule
+    MessagesComponent,
+    DashboardComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
